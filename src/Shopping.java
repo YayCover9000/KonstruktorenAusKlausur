7d67028eeac1011439df10f1480fcd362f4bb92f
YayCover9000 <yassuanf@googlemail.com>
2023-03-16 16:33:21 +0100

    add Instanzkonstruktor

--- src/Shopping.java
+++ src/Shopping.java
@@ -2,7 +2,16 @@ public class Shopping {
     //Bitte Insanzvariablen nicht verändern
     float weight;
     float price;
-    public static void main (String [] args) {
 
+    // 1.) Konstruktor erstellen
+    Shopping (float weightValue, float priceValue) {
+        weight = weightValue;
+        price = priceValue;
+        System.out.println("Das ist der Initialisierungskonstruktor");
     }
+    // Wichtig die Methode "erwartet" keinen Wert
+    void ausgebenWert () {
+        System.out.println("Weight: " + weight + "\nPrice: " + price);
+    }
+
 }
---
+++ src/ShoppingTesttreiber.java
@@ -0,0 +1,7 @@
+public class ShoppingTesttreiber {
+    public static void main (String [] args) {
+        // Objekt erstellen (Instanz)
+        Shopping wert = new Shopping(15,51);
+        wert.ausgebenWert();
+    }
+}
